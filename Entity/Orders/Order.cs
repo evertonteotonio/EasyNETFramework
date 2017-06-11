2c7c2e42b5f81288d09743ab6c03ae011d843f3e
amr.swalha.code <amr.swalha.code@outlook.com>
2017-06-11 12:25:49 +0300

    image + order entity

---
+++ Entity/Orders/Order.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace ENF.Entity.Orders
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string OrderTitle { get; set; }
+        public decimal Amount { get; set; }
+        public string CustomerName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
